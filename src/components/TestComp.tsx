97a4d5c571d681ffff621eb7016d1a632f310206
d0uwhs <d0uwhs@gmail.com>
2022-10-25 14:11:02 +0900

    Init Project & Add .gitignore

---
+++ src/components/TestComp.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TestComp = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default TestComp;
---
+++ src/pages/Todolist/TodolistPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodolistPage = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default TodolistPage;
